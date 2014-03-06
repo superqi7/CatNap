f32ac54407e4688512b26e7535b9da21480028ff
Xiaoqi Liu <xiaoqiliu7@gmail.com>
2014-03-05 21:02:00 -0500

    Initial Commit

---
+++ CatNap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CatNap
+//
+//  Created by Xiaoqi Liu on 3/5/14.
+//  Copyright (c) 2014 Xiaoqi Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CatNap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CatNap
+//
+//  Created by Xiaoqi Liu on 3/5/14.
+//  Copyright (c) 2014 Xiaoqi Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
